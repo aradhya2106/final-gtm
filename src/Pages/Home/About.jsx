113b7e44d544ec1474536f31e3d8df29b36d57a4
Bhargav <vyasb1503@gmail.com>
2025-07-10 00:06:56 +0530

    added the changes to the grid

--- src/Pages/Home/About.jsx
+++ src/Pages/Home/About.jsx
@@ -2,6 +2,10 @@ import { Container } from "../../Components/Continer/Continer";
 import { Typography } from "../../Components/Typography/Typography";
 import { Grid, GridItem } from "../../Components/Grid/Grid";
 import "./About.scss";
+import Freampeople from "../../Assets/Freampeople.png"
+import  circular from "../../Assets/circular.png"
+import  rocket from  "../../Assets/rocket.png"
+// import  internet from "../../Assets/Internet.png"
 // import rocket from "../../Assets/rocket.svg";
 // import Frame from "../../Assets/Frame.png";
 // import Vector from "../../Assets/Vector.png";
@@ -27,6 +31,38 @@ export const About = () => {
             Here’s how we help Web3 and AI startups grow with purpose.
           </Typography>
         </Typography>
+      </div>
+      <div>
+      <div class="cards-container">
+  {/* <!-- Left Column --> */}
+  <div class="column">
+    <div class="card card-1">
+      <img src="Freampeople" alt="" class="card-icon" />
+      <h3>Web3 & AI Native Team</h3>
+      <p>Built for Web3. We speak the language of DeFi, DAOs, …</p>
+    </div>
+    <div class="card card-3">
+      <img src="rocket" alt="" class="card-icon" />
+      <h3>Fundraising & Launchpad Support</h3>
+      <p>We support your raise with sharp GTM strategy, decks, …</p>
+    </div>
+  </div>
+
+  {/* <!-- Right Column --> */}
+  <div class="column">
+    <div class="card card-2">
+      <img src="circular" alt="" class="card-icon" />
+      <h3>Outcome‑Driven Approach</h3>
+      <p>We build campaigns that drive real business …</p>
+    </div>
+    <div class="card card-4">
+      <img src="internet" alt="" class="card-icon" />
+      <h3>Global Network Access</h3>
+      <p>We launch globally, activate locally. With on‑ground partners …</p>
+    </div>
+  </div>
+</div>
+
       </div>
     </div>
   );
